80819d14ec6a3767203a3210f36ce108b23f6076
Keshavsharma519 <keshav519sharma@gmail.com>
2022-08-29 16:42:56 +0530

    j-1 = j

--- BubbleSort.java
+++ BubbleSort.java
@@ -10,7 +10,7 @@ public class BubbleSort
 		                 for(int j=1; j < (n-i); j++){  
 		                          if(arr[j-1] > arr[j]){  
 		                                 //swap elements  
-		                                 temp = arr[j-1];  
+		                                 temp = arr[j];  
 		                                 arr[j-1] = arr[j];  
 		                                 arr[j] = temp;  
 		                         }  
